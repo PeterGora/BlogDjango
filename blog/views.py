75b24335d48aaeff07d27431b86b06cd0c5fbeb6
PeterGora <pgoraczko@hotmail.com>
2021-03-02 01:09:51 +0100

    Inital commit

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from models import Category, Post, Comment
 
 
 def index(request):
+    # lista opublikowanych wpisów posortowana od najnowszych do najstarszych
     return HttpResponse("Welcome to my  Blogoshpere!")
+
+def login(request):
+    pass
+
+def logout(request):
+    pass
+
+def register(request):
+    pass
